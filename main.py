d0c0d17ad6b136f0b5a8abee5dd208c5d99b2516
Maximovex <maximove81@gmail.com>
2025-08-29 19:02:33 +0300

    Modified GUI, added QInputdialog for getting root password, added sliders for custom mode - not implemented in code yet

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
-from PySide6.QtWidgets import QApplication,QButtonGroup
+from PySide6.QtWidgets import QApplication,QButtonGroup,QInputDialog
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
 import subprocess
 
 app=QApplication()
 loader=QUiLoader()
-file=QFile('fangui.ui')
+file=QFile('fangui2.ui')
 
 window=loader.load(file)
 file.close
 
-password=window.password_input.text()
-#password='Kf8gCe25!@'
+text,ok = QInputDialog.getText(None,"You need root privileges","Password:")
+if ok:
+    password=text
 
 fixed_speed=window.spinBox.value()
 
+
 def setmode():
-    password=window.password_input.text()
-    if password:
+    keyaccess=password
+    if keyaccess:
         if window.silent_mode.isChecked():
-            subprocess.run(f"echo {password}|sudo -S sh -c 'echo '0' > /sys/devices/platform/aorus_laptop/fan_mode'",capture_output=True,shell=True)
-        if window.normal_mode.isChecked():
-            subprocess.run(f"echo {password}|sudo -S sh -c 'echo '1' > /sys/devices/platform/aorus_laptop/fan_mode'",capture_output=True,shell=True)
+            mode=0
+        elif window.normal_mode.isChecked():
+            mode=1
         elif window.gaming_mode.isChecked():
-            subprocess.run(f"echo {password}|sudo -S sh -c 'echo '2' > /sys/devices/platform/aorus_laptop/fan_mode'",capture_output=True,shell=True)
+            mode=2
         elif window.custom_mode.isChecked():
-            subprocess.run(f"echo {password}|sudo -S sh -c 'echo '3' > /sys/devices/platform/aorus_laptop/fan_mode'",capture_output=True,shell=True)
+            mode=3
         elif window.auto_mode.isChecked():
-            subprocess.run(f"echo {password}|sudo -S sh -c 'echo '4' > /sys/devices/platform/aorus_laptop/fan_mode'",capture_output=True,shell=True)
+            mode=4
         elif window.fixed_mode.isChecked():
-            subprocess.run(f"echo {password}|sudo -S sh -c 'echo '5' > /sys/devices/platform/aorus_laptop/fan_mode'",capture_output=True,shell=True)
+            mode=5
             setspeed()
 
+        com = subprocess.run(f"echo {keyaccess}|sudo -S sh -c 'echo '{mode}' > /sys/devices/platform/aorus_laptop/fan_mode'",capture_output=True,shell=True)
+        print(com.returncode)
+
+        
+
 def setspeed():
-    password=window.password_input.text()
+    keyaccess=password
     fixed_speed=window.spinBox.value()
-    subprocess.run(f"echo {password}|sudo -S sh -c 'echo '{fixed_speed}' > /sys/devices/platform/aorus_laptop/fan_custom_speed'",capture_output=True,shell=True)
+    subprocess.run(f"echo {keyaccess}|sudo -S sh -c 'echo '{fixed_speed}' > /sys/devices/platform/aorus_laptop/fan_custom_speed'",capture_output=True,shell=True)
     
 def chargelimit():
-    password=window.password_input.text()
+    keyaccess=password
     charge_limit=window.charging_limit.value()
     if password:
-        subprocess.run(f"echo {password}|sudo -S sh -c 'echo '{charge_limit}' > /sys/devices/platform/aorus_laptop/charge_limit'",capture_output=True,shell=True)
+        subprocess.run(f"echo {keyaccess}|sudo -S sh -c 'echo '{charge_limit}' > /sys/devices/platform/aorus_laptop/charge_limit'",capture_output=True,shell=True)
 
 window.applymode_btn.clicked.connect(setmode)
 window.applylimit_btn.clicked.connect(chargelimit)
